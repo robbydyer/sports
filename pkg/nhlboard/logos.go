279b6102f6d6864883e280a70e4c5f735348dd3f
rdyer <rdyer@rsglab.com>
2021-01-23 23:14:02 -0500

    wip

--- pkg/nhlboard/logos.go
+++ pkg/nhlboard/logos.go
@@ -58,348 +58,348 @@ func getLogos() (map[string]*logoInfo, error) {
 
 	l := make(map[string]*logoInfo)
 
-	readers, err := logoSources()
+	sources, err := logoSources()
 	if err != nil {
 		return nil, err
 	}
 
 	l[ANA+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 0.8),
+		logo:      logo.New(ANA, sources[ANA], logoCacheDir, 0.8),
 		xPosition: -22,
 		yPosition: 3,
 	}
 	l[ANA+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 0.8),
+		logo:      logo.New(ANA, sources[ANA], logoCacheDir, 0.8),
 		xPosition: 7,
 		yPosition: 3,
 	}
 
 	l[ARI+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(ARI, sources[ARI], logoCacheDir, 1),
 		xPosition: -14,
 		yPosition: 0,
 	}
 	l[ARI+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(ARI, sources[ARI], logoCacheDir, 1),
 		xPosition: 3,
 		yPosition: 0,
 	}
 
 	l[BOS+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(BOS, sources[BOS], logoCacheDir, 1),
 		xPosition: -4,
 		yPosition: 0,
 	}
 	l[BOS+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(BOS, sources[BOS], logoCacheDir, 1),
 		xPosition: 4,
 		yPosition: 0,
 	}
 
 	l[BUF+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(BUF, sources[BUF], logoCacheDir, 1),
 		xPosition: -4,
 		yPosition: 0,
 	}
 	l[BUF+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(BUF, sources[BUF], logoCacheDir, 1),
 		xPosition: 4,
 		yPosition: 0,
 	}
 
 	l[CGY+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(CGY, sources[CGY], logoCacheDir, 1),
 		xPosition: -16,
 		yPosition: 0,
 	}
 	l[CGY+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(CGY, sources[CGY], logoCacheDir, 1),
 		xPosition: 5,
 		yPosition: 0,
 	}
 
 	l[CAR+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(CAR, sources[CAR], logoCacheDir, 1),
 		xPosition: -16,
 		yPosition: 0,
 	}
 	l[CAR+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(CAR, sources[CAR], logoCacheDir, 1),
 		xPosition: 15,
 		yPosition: 0,
 	}
 
 	l[CHI+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(CHI, sources[CHI], logoCacheDir, 1),
 		xPosition: -4,
 		yPosition: 0,
 	}
 	l[CHI+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(CHI, sources[CHI], logoCacheDir, 1),
 		xPosition: 20,
 		yPosition: 0,
 	}
 
 	l[COL+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(COL, sources[COL], logoCacheDir, 1),
 		xPosition: -5,
 		yPosition: 0,
 	}
 	l[COL+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(COL, sources[COL], logoCacheDir, 1),
 		xPosition: 5,
 		yPosition: 0,
 	}
 
 	l[CBJ+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(CBJ, sources[CBJ], logoCacheDir, 1),
 		xPosition: -15,
 		yPosition: 0,
 	}
 	l[CBJ+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(CBJ, sources[CBJ], logoCacheDir, 1),
 		xPosition: 8,
 		yPosition: 0,
 	}
 
 	l[DAL+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(DAL, sources[DAL], logoCacheDir, 1),
 		xPosition: -10,
 		yPosition: 0,
 	}
 	l[DAL+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(DAL, sources[DAL], logoCacheDir, 1),
 		xPosition: 12,
 		yPosition: 0,
 	}
 
 	l[DET+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(DET, sources[DET], logoCacheDir, 1),
 		xPosition: -5,
 		yPosition: 0,
 	}
 	l[DET+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(DET, sources[DET], logoCacheDir, 1),
 		xPosition: 24,
 		yPosition: 0,
 	}
 
 	l[EDM+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(EDM, sources[EDM], logoCacheDir, 1),
 		xPosition: -3,
 		yPosition: 0,
 	}
 	l[EDM+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(EDM, sources[EDM], logoCacheDir, 1),
 		xPosition: 3,
 		yPosition: 0,
 	}
 
 	l[FLA+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(FLA, sources[FLA], logoCacheDir, 1),
 		xPosition: -3,
 		yPosition: 0,
 	}
 	l[FLA+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(FLA, sources[FLA], logoCacheDir, 1),
 		xPosition: 3,
 		yPosition: 0,
 	}
 
 	l[LAK+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 0.9),
+		logo:      logo.New(LAK, sources[LAK], logoCacheDir, 0.9),
 		xPosition: -1,
 		yPosition: 2,
 	}
 	l[LAK+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 0.9),
+		logo:      logo.New(LAK, sources[LAK], logoCacheDir, 0.9),
 		xPosition: 1,
 		yPosition: 2,
 	}
 
 	l[MIN+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(MIN, sources[MIN], logoCacheDir, 1),
 		xPosition: -20,
 		yPosition: 0,
 	}
 	l[MIN+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(MIN, sources[MIN], logoCacheDir, 1),
 		xPosition: 13,
 		yPosition: 0,
 	}
 
 	l[MTL+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 0.9),
+		logo:      logo.New(MTL, sources[MTL], logoCacheDir, 0.9),
 		xPosition: -3,
 		yPosition: 2,
 	}
 	l[MTL+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 0.9),
+		logo:      logo.New(MTL, sources[MTL], logoCacheDir, 0.9),
 		xPosition: 3,
 		yPosition: 2,
 	}
 
 	l[NJD+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(NJD, sources[NJD], logoCacheDir, 1),
 		xPosition: -4,
 		yPosition: 0,
 	}
 	l[NJD+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(NJD, sources[NJD], logoCacheDir, 1),
 		xPosition: 4,
 		yPosition: 0,
 	}
 
 	l[NSH+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(NSH, sources[NSH], logoCacheDir, 1),
 		xPosition: -4,
 		yPosition: 0,
 	}
 	l[NSH+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(NSH, sources[NSH], logoCacheDir, 1),
 		xPosition: 4,
 		yPosition: 0,
 	}
 
 	l[NYI+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(NYI, sources[NYI], logoCacheDir, 1),
 		xPosition: -3,
 		yPosition: 0,
 	}
 	l[NYI+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(NYI, sources[NYI], logoCacheDir, 1),
 		xPosition: 3,
 		yPosition: 0,
 	}
 
 	l[NYR+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(NYR, sources[NYR], logoCacheDir, 1),
 		xPosition: -3,
 		yPosition: 0,
 	}
 	l[NYR+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(NYR, sources[NYR], logoCacheDir, 1),
 		xPosition: 3,
 		yPosition: 0,
 	}
 
 	l[OTT+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(OTT, sources[OTT], logoCacheDir, 1),
 		xPosition: -22,
 		yPosition: 0,
 	}
 	l[OTT+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(OTT, sources[OTT], logoCacheDir, 1),
 		xPosition: 3,
 		yPosition: 0,
 	}
 
 	l[PHI+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 0.9),
+		logo:      logo.New(PHI, sources[PHI], logoCacheDir, 0.9),
 		xPosition: -21,
 		yPosition: 2,
 	}
 	l[PHI+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 0.9),
+		logo:      logo.New(PHI, sources[PHI], logoCacheDir, 0.9),
 		xPosition: 8,
 		yPosition: 2,
 	}
 
 	l[PIT+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(PIT, sources[PIT], logoCacheDir, 1),
 		xPosition: -3,
 		yPosition: 0,
 	}
 	l[PIT+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(PIT, sources[PIT], logoCacheDir, 1),
 		xPosition: 18,
 		yPosition: 0,
 	}
 
 	l[SJS+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(SJS, sources[SJS], logoCacheDir, 1),
 		xPosition: -16,
 		yPosition: 0,
 	}
 	l[SJS+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(SJS, sources[SJS], logoCacheDir, 1),
 		xPosition: 7,
 		yPosition: 0,
 	}
 
 	l[STL+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(STL, sources[STL], logoCacheDir, 1),
 		xPosition: -4,
 		yPosition: 0,
 	}
 	l[STL+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(STL, sources[STL], logoCacheDir, 1),
 		xPosition: 20,
 		yPosition: 0,
 	}
 
 	l[TBL+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(TBL, sources[TBL], logoCacheDir, 1),
 		xPosition: -3,
 		yPosition: 0,
 	}
 	l[TBL+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(TBL, sources[TBL], logoCacheDir, 1),
 		xPosition: 5,
 		yPosition: 0,
 	}
 
 	l[TOR+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(TOR, sources[TOR], logoCacheDir, 1),
 		xPosition: -4,
 		yPosition: 0,
 	}
 	l[TOR+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(TOR, sources[TOR], logoCacheDir, 1),
 		xPosition: 4,
 		yPosition: 0,
 	}
 
 	l[VAN+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(VAN, sources[VAN], logoCacheDir, 1),
 		xPosition: -17,
 		yPosition: 0,
 	}
 	l[VAN+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(VAN, sources[VAN], logoCacheDir, 1),
 		xPosition: 5,
 		yPosition: 0,
 	}
 
 	l[VGK+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(VGK, sources[VGK], logoCacheDir, 1),
 		xPosition: -2,
 		yPosition: 0,
 	}
 	l[VGK+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(VGK, sources[VGK], logoCacheDir, 1),
 		xPosition: 2,
 		yPosition: 0,
 	}
 
 	l[WSH+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1.2),
+		logo:      logo.New(WSH, sources[WSH], logoCacheDir, 1.2),
 		xPosition: -5,
 		yPosition: 3,
 	}
 	l[WSH+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1.2),
+		logo:      logo.New(WSH, sources[WSH], logoCacheDir, 1.2),
 		xPosition: 40,
 		yPosition: -3,
 	}
 
 	l[WPG+"_HOME"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(WPG, sources[WPG], logoCacheDir, 1),
 		xPosition: -2,
 		yPosition: 0,
 	}
 	l[WPG+"_AWAY"] = &logoInfo{
-		logo:      logo.New(NYI, readers[NYI], logoCacheDir, 1),
+		logo:      logo.New(WPG, sources[WPG], logoCacheDir, 1),
 		xPosition: 2,
 		yPosition: 0,
 	}
@@ -414,6 +414,7 @@ func logoSources() (map[string]image.Image, error) {
 		if err != nil {
 			return nil, fmt.Errorf("failed to locate logo asset: %w", err)
 		}
+		defer f.Close()
 
 		i, err := png.Decode(f)
 		if err != nil {
--- pkg/nhlboard/nhlboard.go
+++ pkg/nhlboard/nhlboard.go
@@ -4,16 +4,13 @@ import (
 	"context"
 	"fmt"
 	"image"
-	"image/color"
 	"strings"
 	"time"
 
 	"github.com/go-co-op/gocron"
-	rgb "github.com/robbydyer/rgbmatrix-rpi"
 
 	"github.com/robbydyer/sports/pkg/board"
 	"github.com/robbydyer/sports/pkg/nhl"
-	"github.com/robbydyer/sports/pkg/rgbrender"
 )
 
 var scorePollRate = 30 * time.Second
@@ -30,11 +27,6 @@ type nhlBoards struct {
 	cancel        chan bool
 }
 
-type scoreBoard struct {
-	controller *nhlBoards
-	liveGame   bool
-}
-
 type Config struct {
 	Delay time.Duration
 }
@@ -43,7 +35,7 @@ func New(ctx context.Context, matrixBounds image.Rectangle, config *Config) ([]b
 	var err error
 
 	controller := &nhlBoards{
-		watchTeams:    []string{"NYI", "MTL", "COL"},
+		watchTeams:    ALL,
 		favoriteTeams: []string{"NYI"},
 		logos:         make(map[string]*logoInfo),
 		logoCache:     make(map[string]image.Image),
@@ -68,9 +60,10 @@ func New(ctx context.Context, matrixBounds image.Rectangle, config *Config) ([]b
 
 	controller.logos, err = getLogos()
 
-	// Intialize logo cache
+	// Initialize logo cache
 	for _, t := range ALL {
 		for _, h := range []string{"_HOME", "_AWAY"} {
+			fmt.Printf("Caching logo for %s\n", t+h)
 			_, err := controller.getLogo(t + h)
 			if err != nil {
 				return nil, fmt.Errorf("failed to get logo thumbnail for %s: %w", t, err)
@@ -91,70 +84,6 @@ func New(ctx context.Context, matrixBounds image.Rectangle, config *Config) ([]b
 
 func (n *nhlBoards) updateGames() {
 	_ = n.api.UpdateGames(context.Background(), nhl.Today())
-}
-
-func (b *scoreBoard) Name() string {
-	return "NHL Scoreboard"
-}
-
-func (b *scoreBoard) HasPriority() bool {
-	return b.liveGame
-}
-
-func (b *scoreBoard) Cleanup() {
-	for _, l := range b.controller.logos {
-		_ = l.logo.Close()
-	}
-}
-
-func (b *scoreBoard) Render(ctx context.Context, matrix rgb.Matrix) error {
-	canvas := rgb.NewCanvas(matrix)
-	canvas.Clear()
-
-	seenTeams := make(map[string]bool)
-
-OUTER:
-	for _, abbrev := range b.controller.watchTeams {
-		seen, ok := seenTeams[abbrev]
-		if ok && seen {
-			continue OUTER
-		}
-		seenTeams[abbrev] = true
-
-		team, err := b.controller.api.TeamFromAbbreviation(abbrev)
-		if err != nil {
-			return err
-		}
-
-		for _, game := range b.controller.api.Games[nhl.Today()] {
-			if game.Teams.Away.Team.ID == team.ID || game.Teams.Home.Team.ID == team.ID {
-				liveGame, err := nhl.GetLiveGame(ctx, game.Link)
-				if err != nil {
-					return fmt.Errorf("failed to get live game status of game: %w", err)
-				}
-				if gameNotStarted(liveGame) {
-					if err := b.RenderUpcomingGame(ctx, canvas, liveGame); err != nil {
-						return err
-					}
-				}
-				if gameIsOver(liveGame) {
-					fmt.Printf("%s game is over\n", team.Name)
-					continue OUTER
-				}
-
-				if err := b.RenderGameUntilOver(ctx, canvas, liveGame); err != nil {
-					return err
-				}
-
-			}
-		}
-		select {
-		case <-ctx.Done():
-		case <-time.After(b.controller.config.Delay):
-		}
-	}
-
-	return nil
 }
 
 func gameNotStarted(game *nhl.LiveGame) bool {
@@ -182,24 +111,16 @@ func gameIsOver(game *nhl.LiveGame) bool {
 	return false
 }
 
-func (b *scoreBoard) isFavorite(abbrev string) bool {
-	for _, t := range b.controller.favoriteTeams {
-		if t == abbrev {
-			return true
-		}
-	}
-
-	return false
-}
-
 // getLogo checks cache first
 func (b *nhlBoards) getLogo(logoKey string) (image.Image, error) {
 	// Check cache first
 	logo, ok := b.logoCache[logoKey]
-	if ok {
+	if ok && logo.Bounds().Dx() > 0 && logo.Bounds().Dy() > 0 {
+		fmt.Printf("found logo cache for %s\n", logoKey)
 		return logo, nil
 	}
 
+	fmt.Printf("generating logo for %s\n", logoKey)
 	var err error
 	b.logoCache[logoKey], err = b.logos[logoKey].logo.GetThumbnail(b.matrixBounds)
 	if err != nil {
@@ -207,123 +128,6 @@ func (b *nhlBoards) getLogo(logoKey string) (image.Image, error) {
 	}
 
 	return b.logoCache[logoKey], nil
-}
-
-func (b *scoreBoard) RenderGameUntilOver(ctx context.Context, canvas *rgb.Canvas, liveGame *nhl.LiveGame) error {
-	isFavorite := b.isFavorite(liveGame.LiveData.Linescore.Teams.Home.Team.Abbreviation) ||
-		b.isFavorite(liveGame.LiveData.Linescore.Teams.Away.Team.Abbreviation)
-
-	if isFavorite {
-		// TODO: Make atomic?
-		b.liveGame = true
-		defer func() { b.liveGame = false }()
-	}
-
-	for {
-		hKey := fmt.Sprintf("%s_HOME", liveGame.LiveData.Linescore.Teams.Home.Team.Abbreviation)
-		aKey := fmt.Sprintf("%s_AWAY", liveGame.LiveData.Linescore.Teams.Away.Team.Abbreviation)
-
-		homeLogo, err := b.controller.getLogo(hKey)
-		if err != nil {
-			return err
-		}
-		awayLogo, err := b.controller.getLogo(aKey)
-		if err != nil {
-			return err
-		}
-
-		homeShift, err := b.controller.logoShift(hKey)
-		if err != nil {
-			return err
-		}
-		awayShift, err := b.controller.logoShift(aKey)
-		if err != nil {
-			return err
-		}
-
-		if err := rgbrender.DrawImage(canvas, homeShift, homeLogo); err != nil {
-			return err
-		}
-		if err := rgbrender.DrawImage(canvas, awayShift, awayLogo); err != nil {
-			return err
-		}
-
-		// This game is live, scoreboard time
-		fmt.Printf("Live Game: %s vs. %s\nScore: %d - %d\n%s period %s\n",
-			liveGame.LiveData.Linescore.Teams.Home.Team.Name,
-			liveGame.LiveData.Linescore.Teams.Away.Team.Name,
-			liveGame.LiveData.Linescore.Teams.Home.Goals,
-			liveGame.LiveData.Linescore.Teams.Away.Goals,
-			periodStr(liveGame.LiveData.Linescore.CurrentPeriod),
-			liveGame.LiveData.Linescore.CurrentPeriodTimeRemaining,
-		)
-
-		if !isFavorite {
-			return nil
-		}
-
-		updated, err := nhl.GetLiveGame(ctx, liveGame.Link)
-		if err != nil {
-			fmt.Printf("failed to update live game: %s", err.Error())
-		}
-
-		if gameIsOver(updated) {
-			return nil
-		}
-
-		if updated != nil {
-			liveGame = updated
-		}
-
-		select {
-		case <-ctx.Done():
-			return nil
-		case <-time.After(scorePollRate):
-		}
-	}
-}
-
-func (b *scoreBoard) RenderUpcomingGame(ctx context.Context, canvas *rgb.Canvas, liveGame *nhl.LiveGame) error {
-	hKey := fmt.Sprintf("%s_HOME", liveGame.LiveData.Linescore.Teams.Home.Team.Abbreviation)
-	aKey := fmt.Sprintf("%s_AWAY", liveGame.LiveData.Linescore.Teams.Away.Team.Abbreviation)
-
-	homeLogo, err := b.controller.getLogo(hKey)
-	if err != nil {
-		return err
-	}
-	awayLogo, err := b.controller.getLogo(aKey)
-	if err != nil {
-		return err
-	}
-
-	homeShift, err := b.controller.logoShift(hKey)
-	if err != nil {
-		return err
-	}
-	awayShift, err := b.controller.logoShift(aKey)
-	if err != nil {
-		return err
-	}
-
-	if err := rgbrender.DrawImage(canvas, homeShift, homeLogo); err != nil {
-		return err
-	}
-	if err := rgbrender.DrawImage(canvas, awayShift, awayLogo); err != nil {
-		return err
-	}
-	wrter, err := rgbrender.DefaultTextWriter()
-	if err != nil {
-		return err
-	}
-
-	center, err := rgbrender.AlignPosition(rgbrender.CenterCenter, canvas.Bounds(), 5, 5)
-	if err != nil {
-		return err
-	}
-
-	wrter.Write(canvas, center, []string{"vs."}, color.Black)
-
-	return nil
 }
 
 func periodStr(period int) string {
