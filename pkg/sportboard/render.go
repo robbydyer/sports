f451b1dc12266341fc478e4d7ff1ad2febb28941
rdyer <rdyer@rsglab.com>
2021-01-28 13:23:19 -0500

    wip

--- pkg/sportboard/render.go
+++ pkg/sportboard/render.go
@@ -76,30 +76,34 @@ func (s *SportBoard) renderLiveGame(ctx context.Context, canvas *rgb.Canvas, liv
 			return err
 		}
 
+		if !isFavorite {
+			return nil
+		}
+
+		var updatedGame Game
+		go func() {
+			updatedGame, err = liveGame.GetUpdate(ctx)
+			if err != nil {
+				s.log.Errorf("failed to update game for board refresh: %s", err.Error())
+				return
+			}
+			liveGame = updatedGame
+		}()
+
 		select {
 		case <-ctx.Done():
 			return fmt.Errorf("context canceled")
 		case <-time.After(s.config.boardDelay):
 		}
 
-		if !isFavorite {
-			return nil
-		}
-
-		updatedGame, err := liveGame.GetUpdate(ctx)
-		if err != nil {
-			return err
-		}
-
-		over, err := updatedGame.IsComplete()
+		over, err := liveGame.IsComplete()
 		if err != nil {
 			return err
 		}
 		if over {
+			s.log.Infof("live game %d is over", liveGame.GetID())
 			return nil
 		}
-
-		liveGame = updatedGame
 	}
 }
 
@@ -112,9 +116,6 @@ func (s *SportBoard) renderUpcomingGame(ctx context.Context, canvas *rgb.Canvas,
 	if err != nil {
 		return err
 	}
-
-	// If this is a favorite team, we'll watch the scoreboard until the game is over
-	isFavorite := (s.isFavorite(awayTeam.GetAbbreviation()) || s.isFavorite(homeTeam.GetAbbreviation())) && s.config.FavoriteSticky
 
 	timeWriter, timeAlign, err := s.getTimeWriter()
 	if err != nil {
@@ -132,60 +133,31 @@ func (s *SportBoard) renderUpcomingGame(ctx context.Context, canvas *rgb.Canvas,
 	}
 	gameTimeStr := gameTime.Local().Format("3:04PM")
 
-	for {
-		if err := s.RenderHomeLogo(canvas, homeTeam.GetAbbreviation()); err != nil {
-			return err
-		}
-		if err := s.RenderAwayLogo(canvas, awayTeam.GetAbbreviation()); err != nil {
-			return err
-		}
-		timeWriter.Write(
-			canvas,
-			timeAlign,
-			[]string{
-				gameTimeStr,
-			},
-			s.config.TimeColor,
-		)
-
-		scoreWriter.Write(
-			canvas,
-			scoreAlign,
-			[]string{
-				"VS",
-			},
-			s.config.ScoreColor,
-		)
-
-		if err := canvas.Render(); err != nil {
-			return err
-		}
-
-		select {
-		case <-ctx.Done():
-			return fmt.Errorf("context canceled")
-		case <-time.After(s.config.boardDelay):
-		}
-
-		if !isFavorite {
-			return nil
-		}
-
-		updatedGame, err := liveGame.GetUpdate(ctx)
-		if err != nil {
-			return err
-		}
-
-		over, err := updatedGame.IsComplete()
-		if err != nil {
-			return err
-		}
-		if over {
-			return nil
-		}
-
-		liveGame = updatedGame
+	if err := s.RenderHomeLogo(canvas, homeTeam.GetAbbreviation()); err != nil {
+		return err
 	}
+	if err := s.RenderAwayLogo(canvas, awayTeam.GetAbbreviation()); err != nil {
+		return err
+	}
+	timeWriter.Write(
+		canvas,
+		timeAlign,
+		[]string{
+			gameTimeStr,
+		},
+		s.config.TimeColor,
+	)
+
+	scoreWriter.Write(
+		canvas,
+		scoreAlign,
+		[]string{
+			"VS",
+		},
+		s.config.ScoreColor,
+	)
+
+	return canvas.Render()
 }
 
 func (s *SportBoard) renderCompleteGame(ctx context.Context, canvas *rgb.Canvas, liveGame Game) error {
@@ -197,9 +169,6 @@ func (s *SportBoard) renderCompleteGame(ctx context.Context, canvas *rgb.Canvas,
 	if err != nil {
 		return err
 	}
-
-	// If this is a favorite team, we'll watch the scoreboard until the game is over
-	isFavorite := (s.isFavorite(awayTeam.GetAbbreviation()) || s.isFavorite(homeTeam.GetAbbreviation())) && s.config.FavoriteSticky
 
 	timeWriter, timeAlign, err := s.getTimeWriter()
 	if err != nil {
@@ -216,58 +185,29 @@ func (s *SportBoard) renderCompleteGame(ctx context.Context, canvas *rgb.Canvas,
 		return err
 	}
 
-	for {
-		if err := s.RenderHomeLogo(canvas, homeTeam.GetAbbreviation()); err != nil {
-			return err
-		}
-		if err := s.RenderAwayLogo(canvas, awayTeam.GetAbbreviation()); err != nil {
-			return err
-		}
-		timeWriter.Write(
-			canvas,
-			timeAlign,
-			[]string{
-				"FINAL",
-			},
-			s.config.TimeColor,
-		)
-
-		scoreWriter.Write(
-			canvas,
-			scoreAlign,
-			[]string{
-				score,
-			},
-			s.config.ScoreColor,
-		)
-
-		if err := canvas.Render(); err != nil {
-			return err
-		}
-
-		select {
-		case <-ctx.Done():
-			return fmt.Errorf("context canceled")
-		case <-time.After(s.config.boardDelay):
-		}
-
-		if !isFavorite {
-			return nil
-		}
-
-		updatedGame, err := liveGame.GetUpdate(ctx)
-		if err != nil {
-			return err
-		}
-
-		over, err := updatedGame.IsComplete()
-		if err != nil {
-			return err
-		}
-		if over {
-			return nil
-		}
-
-		liveGame = updatedGame
+	if err := s.RenderHomeLogo(canvas, homeTeam.GetAbbreviation()); err != nil {
+		return err
 	}
+	if err := s.RenderAwayLogo(canvas, awayTeam.GetAbbreviation()); err != nil {
+		return err
+	}
+	timeWriter.Write(
+		canvas,
+		timeAlign,
+		[]string{
+			"FINAL",
+		},
+		s.config.TimeColor,
+	)
+
+	scoreWriter.Write(
+		canvas,
+		scoreAlign,
+		[]string{
+			score,
+		},
+		s.config.ScoreColor,
+	)
+
+	return canvas.Render()
 }
