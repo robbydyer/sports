c3e76a4eca6e2ff78408287201e77c9c3a73bdbd
rdyer <rdyer@rsglab.com>
2021-01-30 00:09:47 -0500

    Fix Today()

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -5,7 +5,7 @@ import "time"
 // Today is sometimes actually yesterday
 func Today() time.Time {
 	if time.Now().Local().Hour() < 4 {
-		return time.Now().AddDate(0, 0, 01).Local()
+		return time.Now().AddDate(0, 0, -1).Local()
 	}
 
 	return time.Now().Local()
