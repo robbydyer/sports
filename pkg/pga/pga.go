c470144664dc5e8a810d4be9dbb636329f46228a
robbydyer <robby.dyer@gmail.com>
2021-03-17 23:34:27 -0400

    wip

--- pkg/pga/pga.go
+++ pkg/pga/pga.go
@@ -3,6 +3,7 @@ package pga
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -12,9 +13,7 @@ import (
 const leaderboardURL = "https://site.web.api.espn.com/apis/site/v2/sports/golf/leaderboard?league=pga"
 
 // PGA ...
-type PGA struct {
-	players []*Player
-}
+type PGA struct{}
 
 type eventDat struct {
 	Events []struct {
@@ -56,12 +55,13 @@ func (p *PGA) StatShortName(stat string) string {
 
 // ListPlayers ...
 func (p *PGA) ListPlayers(ctx context.Context, teamAbbreviation string) ([]statboard.Player, error) {
-	if err := p.updatePlayers(ctx); err != nil {
+	plyrs, err := p.updatePlayers(ctx)
+	if err != nil {
 		return nil, err
 	}
 
 	players := []statboard.Player{}
-	for _, player := range p.players {
+	for _, player := range plyrs {
 		players = append(players, player)
 	}
 	return players, nil
@@ -82,10 +82,10 @@ func (p *PGA) PlayerCategories() []string {
 	return []string{"player"}
 }
 
-func (p *PGA) updatePlayers(ctx context.Context) error {
+func (p *PGA) updatePlayers(ctx context.Context) ([]*Player, error) {
 	req, err := http.NewRequest("GET", leaderboardURL, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	req = req.WithContext(ctx)
 
@@ -93,26 +93,26 @@ func (p *PGA) updatePlayers(ctx context.Context) error {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	var dat *eventDat
 
 	if err := json.Unmarshal(body, &dat); err != nil {
-		return err
+		return nil, err
 	}
 
 	for _, event := range dat.Events {
 		for _, comp := range event.Competitions {
-			p.players = comp.Competitors
+			return comp.Competitors, nil
 		}
 	}
 
-	return nil
+	return nil, fmt.Errorf("could not find players")
 }
