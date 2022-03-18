0a0d17e0d3e9157df4aba940bf1b1b6e4ab91671
Robby Dyer <robbydyer@users.noreply.github.com>
2022-03-18 09:45:44 -0400

    Deprecate armv6 arch (#231)

--- script/install.sh
+++ script/install.sh
@@ -5,13 +5,28 @@ set -x
 
 ARCH="$(uname -m)"
 
-latesturl="https://api.github.com/repos/robbydyer/sports/releases/latest"
-
 tmp="$(mktemp -d /tmp/sportsinstall.XXXX)"
 trap "rm -rf ${tmp}" EXIT
 
 cd "${tmp}"
-curl -s "${latesturl}" | grep browser_download_url | grep deb | cut -d: -f2,3 | tr -d \" | wget -qi -
+
+case "${ARCH}" in
+aarch64|armv7l)
+  echo "Installing sportsmatrix for ${ARCH}"
+  latesturl="https://api.github.com/repos/robbydyer/sports/releases/latest"
+
+  curl -s "${latesturl}" | grep browser_download_url | grep deb | cut -d: -f2,3 | tr -d \" | wget -qi -
+  ;;
+armv6l)
+  echo "This architecture is deprecated! The last supported version is v0.0.83"
+  wget -q "https://github.com/robbydyer/sports/releases/download/v0.0.83/sportsmatrix-0.0.83_armv6l.deb"
+  ;;
+*)
+  echo "Unsupported architecture '${ARCH}'"
+  exit 1
+  ;;
+esac
+
 
 sudo dpkg -i --force-confdef --force-confold sportsmatrix*_${ARCH}.deb
 
