0fc71e7c25a6a3d179b124f768a65fceac4151b0
robbydyer <robby.dyer@gmail.com>
2022-05-14 22:44:25 -0400

    wip

--- internal/gcal/cache.go
+++ internal/gcal/cache.go
@@ -7,6 +7,7 @@ import (
 	calendar "google.golang.org/api/calendar/v3"
 )
 
+// getEvents first checks for unexpired cached events
 func (g *Gcal) getEvents(ctx context.Context, calendarID string, date time.Time) ([]*calendar.Event, error) {
 	g.Lock()
 	thisCal, ok := g.calendars[calendarID]
@@ -15,20 +16,21 @@ func (g *Gcal) getEvents(ctx context.Context, calendarID string, date time.Time)
 	}
 	g.Unlock()
 
-	if thisCal == nil || time.Since(thisCal.lastUpdate) > g.refresh {
-		// fetch events from API
-		calEvents, err := g.service.Events.List(calendarID).Context(ctx).TimeMin(dateMin(date)).TimeMax(dateMax(date)).Do()
-		if err != nil {
-			return nil, err
-		}
-		g.Lock()
-		g.calendars[calendarID] = &cal{
-			events:     calEvents.Items,
-			lastUpdate: time.Now(),
-		}
-		thisCal = g.calendars[calendarID]
-		g.Unlock()
+	if thisCal != nil && time.Since(thisCal.lastUpdate) < g.refresh {
+		return thisCal.events, nil
 	}
 
-	return thisCal.events, nil
+	// fetch events from API
+	calEvents, err := g.service.Events.List(calendarID).Context(ctx).TimeMin(dateMin(date)).TimeMax(dateMax(date)).Do()
+	if err != nil {
+		return nil, err
+	}
+	g.Lock()
+	g.calendars[calendarID] = &cal{
+		events:     calEvents.Items,
+		lastUpdate: time.Now(),
+	}
+	g.Unlock()
+
+	return calEvents.Items, nil
 }
