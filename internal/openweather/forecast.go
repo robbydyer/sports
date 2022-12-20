3bb7b086ce455eec97680f8860f2055047686015
Robby Dyer <robbydyer@users.noreply.github.com>
2022-12-20 12:09:12 -0500

    Handle new Openweathermap accounts (#463)
    * Handle new Openweathermap accounts
    
    * wip

--- internal/openweather/forecast.go
+++ internal/openweather/forecast.go
@@ -8,16 +8,19 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
 	"time"
 
 	"go.uber.org/zap"
 
 	weatherboard "github.com/robbydyer/sports/internal/board/weather"
 	"github.com/robbydyer/sports/internal/rgbrender"
+	"github.com/robbydyer/sports/internal/util"
 )
 
 func (w *weather) expired(refresh time.Duration) bool {
-	return w.lastUpdate.Add(refresh).Before(time.Now().Local())
+	return w.LastUpdate.Add(refresh).Before(time.Now().Local())
 }
 
 func (a *API) boardForecastFromForecast(forecasts []*forecast, bounds image.Rectangle, metric bool) ([]*weatherboard.Forecast, error) {
@@ -99,6 +102,59 @@ func (a *API) weatherFromCache(key string) *weather {
 		return w
 	}
 
+	// Try disk cache
+	cDir, err := cacheDir(dataCacheDir)
+	if err != nil {
+		a.log.Error("failed to get cache dir for weather",
+			zap.Error(err),
+		)
+		return nil
+	}
+
+	cacheFile := filepath.Join(cDir, key)
+	exists, err := util.FileExists(cacheFile)
+	if err != nil {
+		a.log.Error("failed to read weather cache file",
+			zap.String("file", cacheFile),
+			zap.Error(err),
+		)
+		return nil
+	}
+	if exists {
+		dat, err := os.ReadFile(cacheFile)
+		if err != nil {
+			a.log.Error("failed to read weather cache file",
+				zap.String("file", cacheFile),
+				zap.Error(err),
+			)
+			return nil
+		}
+		var w *weather
+
+		if err := json.Unmarshal(dat, &w); err != nil {
+			a.log.Error("failed to unmarshal cached weather data",
+				zap.String("file", cacheFile),
+				zap.Error(err),
+			)
+		}
+		if w == nil {
+			a.log.Error("weather cache data was nil",
+				zap.String("file", cacheFile),
+			)
+
+			return w
+		}
+
+		a.log.Info("got weather data from cache file",
+			zap.String("file", cacheFile),
+			zap.String("key", key),
+		)
+
+		// cache to memory so we don't read the file next time
+		a.cache[key] = w
+		return w
+	}
+
 	a.log.Error("cache miss on weather",
 		zap.String("key", key),
 	)
@@ -111,6 +167,32 @@ func (a *API) setWeatherCache(key string, w *weather) {
 	defer a.forecastLock.Unlock()
 
 	a.cache[key] = w
+
+	// Save cache to file
+	cDir, err := cacheDir(dataCacheDir)
+	if err != nil {
+		a.log.Error("failed to get weather data cache dir",
+			zap.Error(err),
+		)
+		return
+	}
+
+	dat, err := json.Marshal(w)
+	if err != nil {
+		a.log.Error("failed marshal weather for caching",
+			zap.Error(err),
+		)
+	}
+
+	cacheFile := filepath.Join(cDir, key)
+	if err := os.WriteFile(cacheFile, dat, 0o644); err != nil {
+		a.log.Error("failed to write weather cache to file",
+			zap.Error(err),
+		)
+	}
+	a.log.Info("wrote weather data to cache file",
+		zap.String("file", cacheFile),
+	)
 }
 
 func (a *API) getWeather(ctx context.Context, zipCode string, country string, metric bool) (*weather, error) {
@@ -122,7 +204,7 @@ func (a *API) getWeather(ctx context.Context, zipCode string, country string, me
 		if w.expired(a.refresh) {
 			a.log.Info("weather cache expired",
 				zap.Float64("minutes", a.refresh.Minutes()),
-				zap.Time("updated", w.lastUpdate),
+				zap.Time("updated", w.LastUpdate),
 			)
 			w = nil
 		} else {
@@ -155,7 +237,7 @@ func (a *API) getWeather(ctx context.Context, zipCode string, country string, me
 		return nil, fmt.Errorf("failed to geolocate")
 	}
 
-	uri, err := url.Parse(fmt.Sprintf("%s/data/2.5/onecall", baseURL))
+	uri, err := url.Parse(fmt.Sprintf("%s/data/%s/onecall", baseURL, a.apiVersion))
 	if err != nil {
 		return nil, err
 	}
@@ -197,6 +279,15 @@ func (a *API) getWeather(ctx context.Context, zipCode string, country string, me
 		return nil, err
 	}
 
+	if resp.StatusCode == http.StatusUnauthorized {
+		a.log.Error("failed to auth to openweathermap.org",
+			zap.Int("status", resp.StatusCode),
+			zap.String("status message", resp.Status),
+			zap.ByteString("data", body),
+		)
+		return nil, fmt.Errorf("failed to get weather data")
+	}
+
 	a.log.Debug("weather data",
 		zap.ByteString("data", body),
 	)
@@ -208,7 +299,7 @@ func (a *API) getWeather(ctx context.Context, zipCode string, country string, me
 	t := time.Now().Local()
 	a.lastAPICall = &t
 
-	w.lastUpdate = time.Now().Local()
+	w.LastUpdate = time.Now().Local()
 
 	a.setWeatherCache(key, w)
 
