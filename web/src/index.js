5bff675cf847b0baeceb7a3a26cb6bd02f5e8af3
robbydyer <robby.dyer@gmail.com>
2024-02-26 13:57:49 -0500

    Fix web

--- web/src/index.js
+++ web/src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
